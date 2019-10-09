4eef8de0331f056ac77cf9572c4d08c1a9ba93ad
Hasan Mahmud <hasan.mahmud8177@gmail.com>
2019-10-10 01:22:51 +0600

    Updated Code

--- BaseModule/BaseModule/urls.py
+++ BaseModule/BaseModule/urls.py
@@ -2,6 +2,7 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('', include('travello.urls')),
     # path('', include('calc.urls')),
--- BaseModule/travello/views.py
+++ BaseModule/travello/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
+from .models import Destination
 
 def index(request):
     # return HttpResponse("Hello World")
-    return render(request,"index.html")
+    
+    dest1 = Destination()
+    dest1.name = "Dhaka"
+    dest1.desc = "I Love Bangladesh"
+    dest1.img = 'destination_1.jpg'
+    dest1.price = 900
+    dest1.offer = True
+
+    dest2 = Destination()
+    dest2.name = "Noakhali"
+    dest2.desc = "I Love Bangladesh"
+    dest2.img = 'destination_1.jpg'
+    dest2.price = 900
+    dest2.offer = False
+
+    dest3 = Destination()
+    dest3.name = "Jessor"
+    dest3.desc = "I Love Bangladesh"
+    dest3.img = 'destination_1.jpg'
+    dest3.price = 900
+    dest3.offer = True
+
+    dests = [dest1, dest2, dest3]
+    return render(request,"index.html", {'dests': dests},)
